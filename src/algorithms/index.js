1aab69a185ccb9db5d7c028d6b586aabdd24b0f6
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-05-18 10:46:42 +0530

    dependecy issue fix t-1

--- src/algorithms/index.js
+++
@@ -1,7 +0,0 @@
-export { default as nearestNeighbour } from './nearestNeighbour';
-export { default as nearestInsertion } from './nearestInsertion';
-export { default as farthestInsertion } from './farthestInsertion';
-export { default as cheapestInsertion } from './cheapestInsertion';
-export { default as randomInsertion } from './randomInsertion';
-export { default as convexHull } from './convexHull';
-export { default as twoOpt } from './twoOpt';
--- src/components/TspVisualizer/index.js
+++
@@ -1,1 +0,0 @@
-export { default as TspVisualiser } from './TspVisualizer';
